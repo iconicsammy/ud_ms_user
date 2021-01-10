ec9bb29abf53355ed2b072d33ba124039c25e2b1
Sium <sium.kiflemariam@oneacrefund.org>
2021-01-10 21:57:54 +0200

    console config

--- src/sequelize.ts
+++ src/sequelize.ts
@@ -1,7 +1,7 @@
 import {Sequelize} from 'sequelize-typescript';
 import {config} from './config/config';
 
-console.log(4, config.password, config.username);
+console.log(4, config);
 export const sequelize = new Sequelize({
   'username': config.username,
   'password': config.password,
